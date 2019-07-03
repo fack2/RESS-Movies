36b68dcf24405387433bad261d0c8c7daae2e9c9
enasmtour <eeenas1997@gmail.com>
2019-07-03 11:42:01 +0300

    update css file and link it to js

--- public/index.js
+++ public/index.js
@@ -17,22 +17,48 @@ var list = document.getElementById("list");
 search.addEventListener("click", function() {
 	name = searchBar.value;
 	request(`http://www.omdbapi.com/?s=${name}&apikey=9cdd68b6`, result => {
-		var title = result.Search.map(ele => ele.Title);
-		var poster = result.Search.map(ele => ele.Poster);
+		var data  = result.Search.map((elm)=>{
+			return{
+				title: elm.Title,
+				poster:elm.Poster
+			}
+		})
+		// var poster = result.Search.map(ele => ele.Poster);
+		console.log(data);
+ data.forEach(element => {
+				 var card = document.createElement("div");
+				 card.classList.add("card");
+				 var img = document.createElement("img");
+				img.setAttribute("src", element.poster);
+				var filmeTitle = document.createElement("h4");
+				filmeTitle.innerHTML= element.title;
+				filmeTitle.classList.add("title");
 
-		if (searchBar.value) {
-			title.forEach(function(ele) {
-				var li = document.createElement("li");
-				li.innerText = ele;
-				list.appendChild(li);
-			});
-			poster.forEach(function(ele) {
-				var img = document.createElement("img");
-				img.setAttribute("src", ele);
-				list.appendChild(img);
-			});
-		} else {
-			search.disabled = true;
-		}
+								 //  card.classList.add("card");
+								 
+list.appendChild(card);
+card.appendChild(img);
+card.appendChild(filmeTitle);
+								 
+
+
+ });
+// 		// if (searchBar.value) {
+		// 	title.forEach(function(ele) { 
+		// 		var card = document.createElement("div");
+		// 		card.classList.add("card");
+		// 		var li = document.createElement("li");
+		// 		li.innerText = ele;
+		// 		list.appendChild(li);
+		// 	});
+		// 	poster.forEach(function(ele) {
+		// 		var img = document.createElement("img");
+		// 		img.setAttribute("src", ele);
+		// 		list.appendChild(img);
+		// 	});
+		// } else {
+		// 	search.disabled = true;
+		// }
+		 
 	});
 });
